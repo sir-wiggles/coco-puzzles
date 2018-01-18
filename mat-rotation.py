f6ae1feb6d9ee81828a7a8fddfdfa83b11f59d7b
Jeff Gregory <jeffmgreg@gmail.com>
2018-01-18 09:44:45 -0800

    update

--- mat-rotation.py
+++ mat-rotation.py
@@ -5,11 +5,12 @@ mat = [['a', 'b'], ['c', 'd']]
 max_r = len(mat) - 1
 max_c = len(mat[0]) - 1
 
+
 for r in range(len(mat) // 2):
 
     ring = []
     c = r
-    vector = [0, 1]  # (x, y)
+    side = 0
     while True:
 
         if (r, c) == ring[0]:
@@ -17,36 +18,36 @@ for r in range(len(mat) // 2):
 
         ring.append((r, c))
 
-        if vector == [0, 1]:
+        if side == 0:
             if r < max_r:
                 r += 1
             else:
                 r = max_r
                 c += 1
-                vector = [1, 0]
+                side = 1
 
-        elif vector == [1, 0]:
+        elif side == 1:
             if c < max_c:
                 c += 1
             else:
                 c = max_c
                 r -= 1
-                vector = [0, -1]
+                side = 2
 
-        elif vector == [0, -1]:
+        elif side == 2:
             if r > 0:
                 r -= 1
             else:
                 r = 0
                 c -= 1
-                vector = [-1, 0]
+                side = 3
 
-        elif vector == [-1, 0]:
+        elif side == 3:
             if c > 0:
                 c -= 1
             else:
                 c = 0
                 r += 1
-                vector = [0, 1]
+                side = 0
 
 
